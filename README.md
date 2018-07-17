9740c6117ba0e0145f307dfd9fa2dbd15b654de2
heaeat <heaeat@naver.com>
2018-07-17 14:02:30 +0000

    Organize files

---
+++ README.md
@@ -0,0 +1,14 @@
+# Handray - Practice
+
+This is repository for practice handray.  
+
+### re1
+
+### re2
+
+### re3
+
+### re4
+
+### re5
+In progress....
---
+++ re5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int list_printall(char *temp){
+	
+
+
+}
+
+int list_init(char *temp, int i){
+
+	
+
+}
+
+int list_add_to_tail(){
+
+}
+
+
+int main(){
+
+	char buf[20];
+	
+	list_init(buf);
+	list_add_to_tail(buf,1);
+	list_add_to_tail(buf,2);
+	list_add_to_tail(buf,3);
+
+	list_printall(buf);
+		
+	return 0;
+}
