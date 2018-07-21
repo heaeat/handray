f878860324d5461147dbb2b4db139f3568e668fe
heaeat <heaeat@naver.com>
2018-07-21 08:07:07 +0000

    Solve re5...Finally!

--- re5.c
+++ re5.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-	struct node *next;
+
+typedef struct _list{
+	struct _node *head;
+	struct _node *tail;
+}list;
+
+typedef struct _node{
 	int data;
-};
+	struct _node *next;
+}node;
 
-struct list{
-	struct node *head;
-	struct node *tail;
 
-};
-
-int list_printall(struct list *temp){
-	struct node *temp_node = temp->head;
-	
-	while(temp_node != NULL){
-		printf("%d\n", temp_node->data);
-	}	
-}
-
-int list_init(struct list *temp){
+int list_init(list *temp){
 	temp->tail = NULL;
 	temp->head = temp->tail;
 }
 
-int list_add_to_tail(struct list *temp, int i){
+int list_add_to_tail(list *temp, int n){
 	
+	if(!temp->tail){
+		temp->tail = malloc(sizeof(node));
+		temp->tail->data = n;
+		temp->tail->next = NULL;
+		temp->head = temp->tail;
+	}else{
+		temp->tail->next = malloc(sizeof(node));
+		temp->tail->next->data = n;
+		temp->tail->next->next = NULL;
+		temp->tail = temp->tail->next;
+	}
+}
+
+int list_printall(list *temp){
+	node *temp_node = temp->head;
+	while(temp_node != NULL){
+		printf("%d\n",temp_node->data);
+		temp_node = temp_node->next;
+	}	
 }
 
 
-int main(){
+int main(void){
+	list my_list;
+	list_init(&my_list);
+	
+	list_add_to_tail(&my_list,1);
+	list_add_to_tail(&my_list,2);
+	list_add_to_tail(&my_list,3);
+	
+	list_printall(&my_list);
 
-	struct node node1;
-	list_init(&node1);
-	list_add_to_tail(&node1,1);
-	list_add_to_tail(&node1,2);
-	list_add_to_tail(&node1,3);
-
-	list_printall(&node1);
-		
 	return 0;
 }
