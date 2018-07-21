1c49a1ec1b6809a13935a423894b4d2138a9c8c7
heaeat <heaeat@naver.com>
2018-07-21 03:42:09 +0000

    solve...ing re5

--- re5.c
+++ re5.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int list_printall(char *temp){
+struct node{
+	struct node *next;
+	int data;
+};
+
+struct list{
+	struct node *head;
+	struct node *tail;
+
+};
+
+int list_printall(struct list *temp){
+	struct node *temp_node = temp->head;
 	
-
-
+	while(temp_node != NULL){
+		printf("%d\n", temp_node->data);
+	}	
 }
 
-int list_init(char *temp, int i){
-
-	
-
+int list_init(struct list *temp){
+	temp->tail = NULL;
+	temp->head = temp->tail;
 }
 
-int list_add_to_tail(){
-
+int list_add_to_tail(struct list *temp, int i){
+	
 }
 
 
 int main(){
 
-	char buf[20];
-	
-	list_init(buf);
-	list_add_to_tail(buf,1);
-	list_add_to_tail(buf,2);
-	list_add_to_tail(buf,3);
+	struct node node1;
+	list_init(&node1);
+	list_add_to_tail(&node1,1);
+	list_add_to_tail(&node1,2);
+	list_add_to_tail(&node1,3);
 
-	list_printall(buf);
+	list_printall(&node1);
 		
 	return 0;
 }
